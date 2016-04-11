676e2fdd91bef3822f7f4786682adb49539f756c
Denis Wolf <denis.wolf@exploros.com>
2016-04-11 22:20:10 +0300

    Intervals: Overlap fix data generator in tests; add red and green tests; add implement the function;

--- intervals.js
+++ intervals.js
@@ -1,3 +1,27 @@
-module.exports.doesOverlap = function(){
-  
+'use strict';
+
+module.exports.doesOverlap = function(left, right){
+  let early = {},
+      later = {};
+
+  //TODO: add format validation
+
+  if (left.s < right.s){
+    early.s = left.s;
+    early.e = left.e;
+    later.s = right.s;
+    later.e = right.e;
+  } else {
+    early.s = right.s;
+    early.e = right.e;
+    later.s = left.s;
+    later.e = left.e;
+  }
+
+  if (early.e <= later.s){
+    return false;
+  } else {
+    return true;
+  }
+
 };
--- spec/intervals.js
+++ spec/intervals.js
@@ -1,4 +1,4 @@
-'use strict';
+  'use strict';
 const expect = require('chai').expect;
 
 const doesOverlap = require('../intervals').doesOverlap;
@@ -8,17 +8,79 @@ const now = Date.now();
 function getInterval(startOffset, endOffset){
   return {
     s: now + startOffset,
-    e: now - endOffset
+    e: now + endOffset
   };
 }
 
-describe('Overlap Function', function() {
-  before(function(){
-    this.earlyInterval = getInterval(-10000, -5000);
-    this.lateInterval = getInterval(0, 1000);
+describe('Overlap Function with two intervals', function() {
+  describe('when the second stars after the first', function(){
+    before(function(){
+      this.firstInterval = getInterval(-10000, -5000);
+      this.secondInterval = getInterval(0, 1000);
+    });
+    it('returns false', function () {
+      const result = doesOverlap(this.firstInterval, this.secondInterval);
+      expect(result).to.be.false;
+    });
   });
-  it('returns false when intervals do not overlap', function () {
-    const result = doesOverlap(this.earlyInterval, this.lateInterval);
-    expect(result).to.be.false;
+  describe('when the first starts after the second', function(){
+    before(function(){
+      this.firstInterval = getInterval(+5000, +1000);
+      this.secondInterval = getInterval(0, 1000);
+    });
+    it('returns false', function () {
+      const result = doesOverlap(this.firstInterval, this.secondInterval);
+      expect(result).to.be.false;
+    });
+  });
+  describe('when overlap partially', function(){
+    before(function(){
+      this.firstInterval = getInterval(-10000, -5000);
+      this.secondInterval = getInterval(-7000, 1000);
+    });
+    it('returns true', function () {
+      const result = doesOverlap(this.firstInterval, this.secondInterval);
+      expect(result).to.be.true;
+    });
+  });
+  describe('when the second is inside the first', function(){
+    before(function(){
+      this.firstInterval = getInterval(-10000, -5000);
+      this.secondInterval = getInterval(-7000, -6000);
+    });
+    it('returns true', function () {
+      const result = doesOverlap(this.firstInterval, this.secondInterval);
+      expect(result).to.be.true;
+    });
+  });
+  describe('when the first is inside the second', function(){
+    before(function(){
+      this.firstInterval = getInterval(-7000, 1000);
+      this.secondInterval = getInterval(-10000, -5000);
+    });
+    it('returns true', function () {
+      const result = doesOverlap(this.firstInterval, this.secondInterval);
+      expect(result).to.be.true;
+    });
+  });
+  describe('when the second is right after the first', function(){
+    before(function(){
+      this.firstInterval = getInterval(-7000, 1000);
+      this.secondInterval = getInterval(1000, 5000);
+    });
+    it('returns false', function () {
+      const result = doesOverlap(this.firstInterval, this.secondInterval);
+      expect(result).to.be.false;
+    });
+  });
+  describe('when the first is the same after the second', function(){
+    before(function(){
+      this.firstInterval = getInterval(1000, 2000);
+      this.secondInterval = getInterval(1000, 2000);
+    });
+    it('returns true', function () {
+      const result = doesOverlap(this.firstInterval, this.secondInterval);
+      expect(result).to.be.true;
+    });
   });
 });
