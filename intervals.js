1bf818e0f6c8adade86f433e7fcecc3115cc1e30
Denis Wolf <denis.wolf@exploros.com>
2016-04-12 01:28:21 +0300

    performance optimization: count intervals only till have enough matches

--- intervals.js
+++ intervals.js
@@ -25,26 +25,43 @@ const doesOverlap = module.exports.doesOverlap = function doesOverlap(left, righ
   }
 };
 
-function getNumberOfOverlappingElements(arr, itemToCheck) {
-  return arr
-    .map(function (currentItem) {
-      return doesOverlap(currentItem, itemToCheck);
-    })
-    .filter((truthy)=>truthy)
-    .length;
+const numberOfOverlaps = module.exports.numberOfOverlaps = function numberOfOverlaps(left, right){
+  let early = {},
+    later = {};
+
+  if (left.s < right.s){
+    early.s = left.s;
+    early.e = left.e;
+    later.s = right.s;
+    later.e = right.e;
+  } else {
+    early.s = right.s;
+    early.e = right.e;
+    later.s = left.s;
+    later.e = left.e;
+  }
+
+  if (early.e <= later.s){
+    return 0;
+  } else {
+    return 1;
+  }
+};
+
+function getNumberOfOverlappingElements(arr, itemToCheck, times) {
+  var overlaps = 0;
+  for (let i=0; (i < arr.length) && (overlaps < times); i++){
+    overlaps += numberOfOverlaps(arr[i],itemToCheck);
+  }
+  return overlaps;
 }
 
 const overlapsNTimes = module.exports.overlapsNTimes = function overlapsNTimes(times){
   return function(arr){
-    let overlapsPerElements = arr.map(function(currentItem){
-      // exclude overlapping with itself
-      return getNumberOfOverlappingElements(arr, currentItem);
-    });
-
-    let sufficientOverlaps = overlapsPerElements.filter(function(overlaps){
-      return overlaps >= times;
-    });
-
-    return sufficientOverlaps.length > 0;
+    var overlaps = 0;
+    for (let i=0; i<(arr.length - 1) && (overlaps < times) ; i++){
+      overlaps = getNumberOfOverlappingElements(arr,arr[i], times);
+    }
+    return overlaps >= times;
   }
 };
